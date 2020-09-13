ed6b1eddf85c519b18ddd01760ac77342e0efab2
Gonets-Artem <70897099+Gonets-Artem@users.noreply.github.com>
2020-09-13 16:56:26 +0300

    Update TestShape.java

--- practice_2/src/com/company/task2num1/TestShape.java
+++ practice_2/src/com/company/task2num1/TestShape.java
@@ -1,7 +1,5 @@
 package com.company.task2num1;
 
-//import com.company.task2num2.Ball;
-
 public class TestShape {
     public static void main(String[] args) {
         Shape shape = new Shape("triangle", 12.2);
