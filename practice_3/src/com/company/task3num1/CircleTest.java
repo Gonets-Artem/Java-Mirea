7269275d7c79a99b553dc7995909d540a195b291
Gonets-Artem <70897099+Gonets-Artem@users.noreply.github.com>
2020-09-14 15:08:16 +0300

    Delete CircleTest.java

--- practice_3/src/com/company/task3num1/CircleTest.java
+++
@@ -1,8 +0,0 @@
-package com.company.task3num1;
-
-public class CircleTest {
-    public static void main(String[] args) {
-        Circle circle = new Circle(9.3);
-        System.out.println("radius: " + circle.getRadius());
-    }
-}
