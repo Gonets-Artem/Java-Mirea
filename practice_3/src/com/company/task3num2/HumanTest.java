6220c87adbcf0fdcd87a2a01b85807c66fefbbc1
Gonets-Artem <70897099+Gonets-Artem@users.noreply.github.com>
2020-09-14 15:09:39 +0300

    Delete HumanTest.java

--- practice_3/src/com/company/task3num2/HumanTest.java
+++
@@ -1,8 +0,0 @@
-package com.company.task3num2;
-
-public class HumanTest {
-    public static void main(String[] args) {
-        Human human = new Human("John", 20, 180, 70, 58, 9, 40);
-        System.out.println(human);
-    }
-}
