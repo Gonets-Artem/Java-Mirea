3ff17b5284121dc09faeeaff81213cf76188b354
Gonets-Artem <70897099+Gonets-Artem@users.noreply.github.com>
2020-09-14 15:10:04 +0300

    Delete BookTest.java

--- practice_3/src/com/company/task3num3/BookTest.java
+++
@@ -1,8 +0,0 @@
-package com.company.task3num3;
-
-public class BookTest {
-    public static void main(String[] args) {
-        Book book = new Book("Война и мир", "Толстой Л.Н.", 1873, 1274, "Русский вестник");
-        System.out.println(book);
-    }
-}
