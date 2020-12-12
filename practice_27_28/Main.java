6a53aa7abe7b2863de8d02f778f3a962869792a7
Argon <70897099+Gonets-Artem@users.noreply.github.com>
2020-12-12 18:30:08 +0300

    Add files via upload

---
+++ practice_27_28/Main.java
@@ -0,0 +1,34 @@
+package practice_27_28;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException, InvocationTargetException, IllegalAccessException {
+        Worker worker = new Worker();
+        Gson gson = new Gson();
+        Type ReflectionTaskType = new TypeToken<ArrayList<ReflectionTask>>(){
+        }.getType();
+        Class<? extends Worker> workerClass = worker.getClass();
+        List<Method> declaredMethods = Arrays.stream(workerClass.getDeclaredMethods())
+                .filter(m -> Arrays.stream(m.getAnnotations()).anyMatch(a -> a instanceof MyAnnotation))
+                .collect(Collectors.toList());
+        ArrayList<ReflectionTask> reflectionTasks = gson.fromJson(Worker.getReflectionTasks(), ReflectionTaskType);
+        for (ReflectionTask reflectionTask : reflectionTasks) {
+            for (Method declaredMethod : declaredMethods) {
+                if (reflectionTask.getType().equals(declaredMethod.getName())) {
+                    declaredMethod.invoke(worker, reflectionTask.getData());
+                }
+            }
+        }
+    }
+}
