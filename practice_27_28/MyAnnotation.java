f810dc23dc11ee24bc9b7f749e455ca71f29fcd2
Argon <70897099+Gonets-Artem@users.noreply.github.com>
2020-12-18 09:42:07 +0300

    Delete MyAnnotation.java

--- practice_27_28/MyAnnotation.java
+++
@@ -1,8 +0,0 @@
-package practice_27_28;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MyAnnotation {
-}
